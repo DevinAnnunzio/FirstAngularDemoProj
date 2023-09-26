baaebf48633c07403596c118bbe39fd768df8387
DevinAnnunzio <annunzio.da@gmail.com>
2023-09-26 14:06:50 +0900

    Feat: created first component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+//meed to pass object to config it.  The name is what you will use when placing in other components html file
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{
+
+}
